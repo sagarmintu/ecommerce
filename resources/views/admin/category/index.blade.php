75662ddca878c4e25292259734b00402fb61a025
sagarmintu <sagarskbehera1996@gmail.com>
2023-05-08 22:57:53 +0530

    fetch data in a table

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -2,8 +2,38 @@
 
 @section('content')
     <div class="card">
+        <div class="card-header">
+            <h4>Category Page</h4>
+            <hr>
+        </div>
         <div class="card-body">
-            <h1>Category Page</h1>
+            <table class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Image</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($category as $item)
+                    <tr>
+                        <td>{{$item->id}}</td>
+                        <td>{{$item->name}}</td>
+                        <td>{{$item->description}}</td>
+                        <td>
+                            <img height="100" width="100" src="{{asset('assets/uploads/category/'.$item->image)}}">
+                        </td>
+                        <td>
+                            <button class="btn btn-success">Edit</button>
+                            <button class="btn btn-danger">Delete</button>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 @endsection
