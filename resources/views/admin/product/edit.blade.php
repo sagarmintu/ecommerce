3a10d4756b2ad02396315715f1dc85197f6e787d
sagarmintu <sagarskbehera1996@gmail.com>
2023-05-16 22:32:00 +0530

    edit and delete products

--- resources/views/admin/product/edit.blade.php
+++ resources/views/admin/product/edit.blade.php
@@ -27,75 +27,76 @@
             <h4>Add Product</h4>
         </div>
         <div class="card-body">
-            <form action="{{url('insert-product')}}" method="POST" enctype="multipart/form-data">
+            <form action="{{url('update-product/'.$products->id)}}" method="POST" enctype="multipart/form-data">
                 @csrf
+                @method('PUT')
                 <div class="row">
                     <div class="col-md-12 mb-3">
                     <label for="">Category</label>
-                    <select class="form-select" name="cate_id">
-                        <option value="">Select A Category</option>
-                        @foreach($category as $item)
-                            <option value="{{ $item->id }}">{{ $item->name }}</option>
-                        @endforeach
+                    <select class="form-select">
+                        <option value="">{{$products->category->name}}</option>
                     </select>
                     </div>
                     <div class="col-md-6 mb-3">
                         <label for="name">Name</label>
-                        <input type="text" name="name" class="form-control">
+                        <input type="text" name="name" class="form-control" value="{{$products->name}}">
                     </div>
                     <div class="col-md-6 mb-3">
                         <label for="slug">Slug</label>
-                        <input type="text" name="slug" class="form-control">
+                        <input type="text" name="slug" class="form-control" value="{{$products->slug}}">
                     </div>
                     <div class="col-md-12 mb-3">
                         <label for="description">Small Description</label>
-                        <textarea name="small_description" rows="3" class="form-control"></textarea>
+                        <textarea name="small_description" rows="3" class="form-control">{{$products->small_description}}</textarea>
                     </div>
                     <div class="col-md-12 mb-3">
                         <label for="description">Description</label>
-                        <textarea name="description" rows="3" class="form-control"></textarea>
+                        <textarea name="description" rows="3" class="form-control">{{$products->description}}</textarea>
                     </div>
                     <div class="col-md-6 mb-3">
                         <label for="original_price">Original Price</label>
-                        <input type="number" name="original_price" class="form-control">
+                        <input type="number" name="original_price" class="form-control" value="{{$products->original_price}}">
                     </div>
                     <div class="col-md-6 mb-3">
                         <label for="selling_price">Selling Price</label>
-                        <input type="number" name="selling_price" class="form-control">
+                        <input type="number" name="selling_price" class="form-control" value="{{$products->selling_price}}">
                     </div>
                     <div class="col-md-6 mb-3">
                         <label for="qty">Quantity</label>
-                        <input type="number" name="qty" class="form-control">
+                        <input type="number" name="qty" class="form-control" value="{{$products->qty}}">
                     </div>
                     <div class="col-md-6 mb-3">
                         <label for="tax">Tax</label>
-                        <input type="number" name="tax" class="form-control">
+                        <input type="number" name="tax" class="form-control" value="{{$products->tax}}">
                     </div>
                     <div class="col-md-6 mb-3">
                         <label for="status">Status</label>
-                        <input type="checkbox" name="status">
+                        <input type="checkbox" name="status" {{ $products->status == '1' ? 'checked' : '' }}>
                     </div>
                     <div class="col-md-6 mb-3">
                         <label for="trending">Trending</label>
-                        <input type="checkbox" name="trending">
+                        <input type="checkbox" name="trending" {{ $products->trending == '1' ? 'checked' : '' }}>
                     </div>
                     <div class="col-md-12 mb-3">
                         <label for="meta_title">Meta Title</label>
-                        <input type="text" name="meta_title" class="form-control">
+                        <input type="text" name="meta_title" class="form-control" value="{{$products->meta_title}}">
                     </div>
                     <div class="col-md-12 mb-3">
                         <label for="meta_keyword">Meta Keywords</label>
-                        <input type="text" name="meta_keyword" class="form-control">
+                        <input type="text" name="meta_keyword" class="form-control" value="{{$products->meta_keyword}}">
                     </div>
                     <div class="col-md-12 mb-3">
                         <label for="meta_description">Meta Description</label>
-                        <textarea name="meta_description" rows="3" class="form-control"></textarea>
+                        <textarea name="meta_description" rows="3" class="form-control">{{$products->meta_description}}</textarea>
                     </div>
+                    @if($products->image)
+                    <img src="{{ asset('assets/uploads/products/'.$products->image) }}" width="100px" height="100px">
+                    @endif
                     <div class="col-md-12">
                         <input type="file" name="image" class="form-control">
                     </div>
                     <div class="col-md-12">
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <button type="submit" class="btn btn-primary">Update</button>
                     </div>
                 </div>
             </form>
--- routes/web.php
+++ routes/web.php
@@ -35,4 +35,7 @@ Route::middleware(['auth','isAdmin'])->group(function(){
     Route::get('products', [ProductController::class, 'index']);
     Route::get('add-products', [ProductController::class, 'add']);
     Route::post('insert-product', [ProductController::class, 'insert']);
+    Route::get('edit-product/{id}', [ProductController::class, 'edit']);
+    Route::put('update-product/{id}', [ProductController::class, 'update']);
+    Route::get('delete-product/{id}', [ProductController::class, 'delete']);
 });
